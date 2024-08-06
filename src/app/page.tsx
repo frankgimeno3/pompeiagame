d9fdcb4e89a816864eb1e7da680bfa125a21e641
frank gimeno <frankgimeno3@gmail.com>
2024-08-06 19:01:53 +0200

    corrijo redirección

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,7 +75,7 @@ const Home = () => {
 
             <div>
               <button
-                onClick={() => signIn('credentials', { email, password, redirect: true, callbackUrl: '/dashboard' })}
+                onClick={() => signIn('credentials', { email, password, redirect: true, callbackUrl: '/landing' })}
                 disabled={!email || !password}
                 className="disabled:opacity-40 flex w-full justify-center rounded-md bg-blue-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500"
               >
