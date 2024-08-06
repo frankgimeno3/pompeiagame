766bba8e684538d5c7927a9a65b9963e425045bb
frank gimeno <frankgimeno3@gmail.com>
2024-08-06 19:40:10 +0200

    meto secretos a salvo en .env

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -4,13 +4,14 @@ import { getFirestore,  } from 'firebase/firestore';
 
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAMJ-npAgY8ry6TUGMQ_wC6H_rsZ-G_P7I",
-  authDomain: "pompeiaquizz.firebaseapp.com",
-  projectId: "pompeiaquizz", 
-  storageBucket: "pompeiaquizz.appspot.com",
-  messagingSenderId: "428224611669",
-  appId: "1:428224611669:web:8d7271c51c5efd76695bdc"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 
