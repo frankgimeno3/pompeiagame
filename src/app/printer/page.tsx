714ef90b79ebef530d96124fde8b5ac37e48860e
frank gimeno <frankgimeno3@gmail.com>
2024-08-21 22:19:35 +0200

    añado printer al mismo archivo

---
+++ src/app/printer/page.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+interface PrinterProps {
+  
+}
+
+const Printer: FC<PrinterProps> = ({ }) => {
+  return (
+    <div>Printer</div>
+  );
+};
+
+export default Printer;
