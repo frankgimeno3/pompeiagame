addaaf641971af46facea5c99d0ae2858729ec2f
frank gimeno <frankgimeno3@gmail.com>
2024-08-23 00:00:51 +0200

    reajusto tamaño titular

--- src/app/landing/comp_landing/0landing/LandingContent.tsx
+++ src/app/landing/comp_landing/0landing/LandingContent.tsx
@@ -23,8 +23,8 @@ const LandingContent: FC<LandingContentProps> = ({ lang, setLang }) => {
   return (
     <div>
             <p className="pt-24 mt-24 text-3xl text-black ">{Content.dashboard.tegustariasaber[lang]}</p>
-            <h1 className="pt-6 text-6xl mx-20">{Content.dashboard.quediosdelolimpo[lang]}</h1>
-      <h1 className="pt-4 text-6xl mb-5 mx-20">{Content.dashboard.terepresenta[lang]}</h1>
+            <h1 className="pt-6 text-5xl mx-20">{Content.dashboard.quediosdelolimpo[lang]}</h1>
+      <h1 className="pt-4 text-5xl mb-5 mx-20">{Content.dashboard.terepresenta[lang]}</h1>
       <p className="text-3xl text-black">{Content.dashboard.unaaventuraquenosensena[lang]}</p>
       <button className="mt-11 px-8 py-4  text-4xl text-black bg-cyan-700 rounded bg-opacity-40 shadow-3xl mx-auto" onClick={() => redirectTo(`/landing/${lang}`)} >
       {Content.dashboard.botonjugar[lang]}
