d898ffc8986e9c9ead42e70324eaf626a8363b02
frank gimeno <frankgimeno3@gmail.com>
2024-08-22 23:58:36 +0200

    estructura flexible para cada lengua

---
+++ src/app/landing/comp_landing/0landing/LandingContent.tsx
@@ -0,0 +1,56 @@
+import React, { FC } from 'react';
+import Image from "next/image";
+import { useRouter } from 'next/navigation';
+import Content from "../../contenidoTotem.json"
+
+interface LandingContentProps {
+    lang: 'es' | 'en' | 'de';  
+    setLang: (lang: 'es' | 'en' | 'de') => void;  
+  }
+
+const LandingContent: FC<LandingContentProps> = ({ lang, setLang }) => {
+
+    const router = useRouter()
+    const redirectTo = (param: string) => {
+      router.push(param)
+    }
+
+    const changeLang = (langChosen: 'es' | 'en' | 'de') => {
+        setLang(langChosen);
+      }
+ 
+
+  return (
+    <div>
+            <p className="pt-24 mt-24 text-3xl text-black ">{Content.dashboard.tegustariasaber[lang]}</p>
+            <h1 className="pt-6 text-6xl mx-20">{Content.dashboard.quediosdelolimpo[lang]}</h1>
+      <h1 className="pt-4 text-6xl mb-5 mx-20">{Content.dashboard.terepresenta[lang]}</h1>
+      <p className="text-3xl text-black">{Content.dashboard.unaaventuraquenosensena[lang]}</p>
+      <button className="mt-11 px-8 py-4  text-4xl text-black bg-cyan-700 rounded bg-opacity-40 shadow-3xl mx-auto" onClick={() => redirectTo(`/landing/${lang}`)} >
+      {Content.dashboard.botonjugar[lang]}
+      </button>
+      <div className="flex justify-center items-center mt-10 mb-10">
+        <div className="flex flex-col text-black mx-5 justify-center text-center ">
+          <div className="rounded-full overflow-hidden">
+            <Image src="/icon/flor.png" alt="flor" width={50} height={50} />
+          </div>
+          <button className="text-3xl text-center " onClick={()=> changeLang("en")}>EN</button>
+        </div>
+        <div className="flex flex-col text-black  mx-5 justify-center text-center ">
+          <div className="rounded-full overflow-hidden">
+            <Image src="/icon/flor.png" alt="flor" width={50} height={50} />
+          </div>
+          <button className="text-3xl text-center " onClick={()=> changeLang("es")}>ES</button>
+        </div>
+        <div className="flex flex-col text-black  mx-5 justify-center text-center ">
+          <div className="rounded-full overflow-hidden">
+            <Image src="/icon/flor.png" alt="flor" width={50} height={50} />
+          </div>
+          <button className="text-3xl text-center " onClick={()=> changeLang("de")}>DE</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingContent;
--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,17 +1,15 @@
 "use client"
 
-import React, { FC } from 'react';
-import Image from "next/image";
-import { useRouter } from 'next/navigation';
+import React, { FC, useState } from 'react';
+import LandingContent from './comp_landing/0landing/LandingContent';
 
 interface LandingProps {
 }
 
 const Landing: FC<LandingProps> = ({ }) => {
-  const router = useRouter()
-  const redirectTo = (param: string) => {
-    router.push(param)
-  }
+
+  const [lang, setLang] = useState<'en' | 'es' | 'de'>('en');
+
   return (
     <div
       className="flex flex-col h-screen mx-auto pt-24 text-center"
@@ -21,34 +19,9 @@ const Landing: FC<LandingProps> = ({ }) => {
         backgroundPosition: "center",
       }}
     >
-      <p className="pt-24 mt-24 text-3xl text-black ">TE GUSTARÍA SABER...</p>
-      <h1 className="pt-6 text-6xl mx-20">¿QUÉ DIOS DEL OLIMPO</h1>
-      <h1 className="pt-4 text-6xl mb-5 mx-20">TE REPRESENTA?</h1>
-      <p className="text-3xl text-black">UNA AVENTURA QUE NOS ENSEÑA LAS CUALIDADES DE CADA DIOS ROMANO</p>
-      <button className="mt-11 px-8 py-4  text-4xl text-black bg-cyan-700 rounded bg-opacity-40 shadow-3xl mx-auto" onClick={() => redirectTo("/landing/es")}>
-        JUGAR
-      </button>
-      <div className="flex justify-center items-center mt-10 mb-10">
-        <div className="flex flex-col text-black mx-5 justify-center text-center " onClick={() => redirectTo("/landing/es")}>
-          <div className="rounded-full overflow-hidden">
-            <Image src="/icon/flor.png" alt="flor" width={50} height={50} />
-          </div>
-          <button className="text-3xl text-center ">EN</button>
-        </div>
-        <div className="flex flex-col text-black  mx-5 justify-center text-center " onClick={() => redirectTo("/landing/es")}>
-          <div className="rounded-full overflow-hidden">
-            <Image src="/icon/flor.png" alt="flor" width={50} height={50} />
-          </div>
-          <button className="text-3xl text-center ">ES</button>
-        </div>
-        <div className="flex flex-col text-black  mx-5 justify-center text-center " onClick={() => redirectTo("/landing/es")}>
-          <div className="rounded-full overflow-hidden">
-            <Image src="/icon/flor.png" alt="flor" width={50} height={50} />
-          </div>
-          <button className="text-3xl text-center ">DE</button>
-        </div>
-      </div>
-    </div>);
+      <LandingContent lang={lang} setLang={setLang}/>
+    </div>
+    );
 };
 
 export default Landing;
