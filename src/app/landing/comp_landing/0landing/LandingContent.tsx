277bd7fbdbe1b0558121d02fdf2e870f553a7eef
frank gimeno <frankgimeno3@gmail.com>
2024-08-23 00:01:51 +0200

    extiendo modificaciones tamaños resto componente landing

--- src/app/landing/comp_landing/0landing/LandingContent.tsx
+++ src/app/landing/comp_landing/0landing/LandingContent.tsx
@@ -22,10 +22,10 @@ const LandingContent: FC<LandingContentProps> = ({ lang, setLang }) => {
 
   return (
     <div>
-            <p className="pt-24 mt-24 text-3xl text-black ">{Content.dashboard.tegustariasaber[lang]}</p>
+            <p className="pt-24 mt-24 text-2xl text-black ">{Content.dashboard.tegustariasaber[lang]}</p>
             <h1 className="pt-6 text-5xl mx-20">{Content.dashboard.quediosdelolimpo[lang]}</h1>
       <h1 className="pt-4 text-5xl mb-5 mx-20">{Content.dashboard.terepresenta[lang]}</h1>
-      <p className="text-3xl text-black">{Content.dashboard.unaaventuraquenosensena[lang]}</p>
+      <p className="text-2xl text-black">{Content.dashboard.unaaventuraquenosensena[lang]}</p>
       <button className="mt-11 px-8 py-4  text-4xl text-black bg-cyan-700 rounded bg-opacity-40 shadow-3xl mx-auto" onClick={() => redirectTo(`/landing/${lang}`)} >
       {Content.dashboard.botonjugar[lang]}
       </button>
@@ -34,19 +34,19 @@ const LandingContent: FC<LandingContentProps> = ({ lang, setLang }) => {
           <div className="rounded-full overflow-hidden">
             <Image src="/icon/flor.png" alt="flor" width={50} height={50} />
           </div>
-          <button className="text-3xl text-center " onClick={()=> changeLang("en")}>EN</button>
+          <button className="text-2xl text-center " onClick={()=> changeLang("en")}>EN</button>
         </div>
         <div className="flex flex-col text-black  mx-5 justify-center text-center ">
           <div className="rounded-full overflow-hidden">
             <Image src="/icon/flor.png" alt="flor" width={50} height={50} />
           </div>
-          <button className="text-3xl text-center " onClick={()=> changeLang("es")}>ES</button>
+          <button className="text-2xl text-center " onClick={()=> changeLang("es")}>ES</button>
         </div>
         <div className="flex flex-col text-black  mx-5 justify-center text-center ">
           <div className="rounded-full overflow-hidden">
             <Image src="/icon/flor.png" alt="flor" width={50} height={50} />
           </div>
-          <button className="text-3xl text-center " onClick={()=> changeLang("de")}>DE</button>
+          <button className="text-2xl text-center " onClick={()=> changeLang("de")}>DE</button>
         </div>
       </div>
     </div>
