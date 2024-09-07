e379ec446e2438a0bcf774349f7bb07b2f579e5d
frank gimeno <frankgimeno3@gmail.com>
2024-09-07 18:06:39 +0200

    paso logo a dcha

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -28,7 +28,7 @@ const Landing: FC<LandingProps> = ({}) => {
       }}
     >
       <LandingContent lang={lang} setLang={setLang} />
-      <div className="absolute bottom-1 left-1/2 transform -translate-x-1/2">
+      <div className="absolute bottom-1 right-1">
   <Copyright />
 </div>
     </div>
