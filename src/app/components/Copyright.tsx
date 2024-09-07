87c4ec13c2246109af582c8395d7c0f58322d360
frank gimeno <frankgimeno3@gmail.com>
2024-09-07 18:04:26 +0200

    actualizacion de fondos, importacion a la derecha de copyright en page

--- src/app/components/Copyright.tsx
+++ src/app/components/Copyright.tsx
@@ -1,13 +1,14 @@
 import React, { FC } from 'react';
 
 interface CopyRightProps {
-  textcolor: any;
 }
 
-const CopyRight: FC<CopyRightProps> = ({textcolor }) => {
+const CopyRight: FC<CopyRightProps> = ({ }) => {
   return (
     <div>
-        <p className='text-center '>© Copyright 2024 ItBringsArt / Info@Itbringsart.com</p>
+<p className='text-center text-xs text-gray-100 font-bold shadow-xl bg-[#b3896d] bg-opacity-50 px-2 py-1'>
+  © Copyright 2024 ItBringsArt / Info@Itbringsart.com
+</p>
     </div>
   );
 };
--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -1,14 +1,14 @@
-"use client"
+"use client";
 
 import React, { FC, useState } from 'react';
 import LandingContent from './comp_landing/0landing/LandingContent';
 import { useSession } from 'next-auth/react';
 import { redirect } from 'next/navigation';
+import Copyright from '../components/Copyright';
 
-interface LandingProps {
-}
+interface LandingProps {}
 
-const Landing: FC<LandingProps> = ({ }) => {
+const Landing: FC<LandingProps> = ({}) => {
   const session = useSession({
     required: true,
     onUnauthenticated() {
@@ -20,16 +20,19 @@ const Landing: FC<LandingProps> = ({ }) => {
 
   return (
     <div
-      className="flex flex-col h-screen mx-auto pt-24 text-center"
+      className="relative flex flex-col h-screen mx-auto pt-24 text-center"
       style={{
         backgroundImage: `url("/fondo1.png")`,
         backgroundSize: "100% 100%",
         backgroundPosition: "center",
       }}
     >
-      <LandingContent lang={lang} setLang={setLang}/>
+      <LandingContent lang={lang} setLang={setLang} />
+      <div className="absolute bottom-1 left-1/2 transform -translate-x-1/2">
+  <Copyright />
+</div>
     </div>
-    );
+  );
 };
 
 export default Landing;
